6e607c1ef0ec64d67fa38a3bf1aacd8001ffba80
Yaroslav Nasonov <38810461+YarikineZ@users.noreply.github.com>
2023-12-12 16:03:45 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+﻿from fastapi import FastAPI, Response
+
+app = FastAPI()
+
+
+@app.get("/legacy/")
+def get_legacy_data():
+    data = """
+[
+  {
+    "id": 0,
+    "progressBarColor": "string",
+    "statusBarColor": "light",
+    "preview": {
+      "previewImageUrl": "string",
+      "previewText": {
+        "textColor": "string",
+        "title": "string",
+        "subTitle": "string",
+        "sizeBetweenTitles": 0
+      }
+    },
+    "storyScreens": [
+      {
+        "imageUrl": "string",
+        "duration": 15,
+        "storyText": {
+          "textColor": "string",
+          "title": "string",
+          "fontSizeTitle": 0,
+          "lineSpacingTitle": 0,
+          "subTitle": "string",
+          "fontSizeSubtitle": 0,
+          "lineSpacingSubtitle": 0,
+          "sizeBetweenTitles": 0,
+          "textPosition": "top",
+          "textAlign": "left"
+        },
+        "storyButton": {
+          "text": "string",
+          "textColor": "string",
+          "buttonColor": "string",
+          "url": "string"
+        }
+      }
+    ],
+    "accessLevel": "allUsers"
+  }
+]
+    """
+    return Response(content=data, media_type="application/json")
