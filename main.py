3f071a26481f8e7ffcf77807a236bcb38e915d9a
Yaroslav Nasonov <38810461+YarikineZ@users.noreply.github.com>
2023-12-26 15:36:06 +0300

    Исправил ошибку /story

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-﻿import json
-from fastapi import FastAPI, Response
+﻿from fastapi import FastAPI, Response
 
 # http://127.0.0.1:8000/docs
 # uvicorn main:app --reload
@@ -9,8 +8,14 @@ app = FastAPI()
 
 @app.get("/story/")
 def get_legacy_data():
-    with open("responce.json", "r") as read_file:
+    try:
+      read_file = open("responce.json", "r")
+      data = read_file.read()
+      read_file.close ()
+
+    except FileNotFoundError:
+      read_file = open("app/responce.json", "r")
       data = read_file.read()
       read_file.close()
-      
+
     return Response(content=data, media_type="application/json")
