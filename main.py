10536ee5c57f9b40105a1aae29ce49b93f31f99d
Yaroslav Nasonov <38810461+YarikineZ@users.noreply.github.com>
2024-01-24 19:42:11 +0300

    поправил за CharGPT

--- main.py
+++ main.py
@@ -22,20 +22,20 @@ def create_story(storyData: schemes.StoryCreate, db: Session = Depends(get_db)):
     db_story = models.Story(**storyData.dict(exclude={"preview", "storyScreens"}))
     db.add(db_story)
 
-    db_preview = models.StoryPreview(story_id=db_story.id, **storyData.preview.dict(exclude={"previewText"}))
+    db_preview = models.StoryPreview(story=db_story, **storyData.preview.dict(exclude={"previewText"}))
     db.add(db_preview)
 
-    db_previewText = models.StoryPreviewText(storyPreview_id=db_preview.id, **storyData.preview.previewText.dict())
+    db_previewText = models.StoryPreviewText(storyPreview=db_preview, **storyData.preview.previewText.dict())
     db.add(db_previewText)
 
     for screen in storyData.storyScreens:
-        db_screen = models.StoryScreen(story_id=db_story.id, **screen.dict(exclude={"storyText", "storyButton"}))
+        db_screen = models.StoryScreen(story=db_story, **screen.dict(exclude={"storyText", "storyButton"}))
         db.add(db_screen)
 
-        db_story_text = models.StoryText(storyScreen_id=db_screen.id, **screen.storyText.dict())
+        db_story_text = models.StoryText(storyScreen=db_screen, **screen.storyText.dict())
         db.add(db_story_text)
 
-        db_story_button = models.StoryButton(storyScreen_id=db_screen.id, **screen.storyButton.dict())
+        db_story_button = models.StoryButton(storyScreen=db_screen, **screen.storyButton.dict())
         db.add(db_story_button)
 
     db.commit()
