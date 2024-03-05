4ccd64c2e8be50b0265c6996a9c694f33ab5842a
Yaroslav Nasonov <38810461+YarikineZ@users.noreply.github.com>
2024-03-05 19:19:51 +0300

    Часть моделей деревом

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
-﻿from fastapi import FastAPI, Response
+﻿from typing import List, Optional
+
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Field, Relationship, Session, SQLModel, create_engine, select
+from models import *
 
 # http://127.0.0.1:8000/docs
 # uvicorn main:app --reload
 
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
 app = FastAPI()
 
 
-@app.get("/story/")
-def get_legacy_data():
-    try:
-      read_file = open("responce.json", "r")
-      data = read_file.read()
-      read_file.close ()
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
-    except FileNotFoundError:
-      read_file = open("app/responce.json", "r")
-      data = read_file.read()
-      read_file.close()
 
-    return Response(content=data, media_type="application/json")
+
+
+@app.get("/stories/all", response_model=List[StoryRead])
+def read_stories(*, session: Session = Depends(get_session)):
+    stories = session.exec(select(Story)).all()
+    return stories
+
---
+++ models.py
@@ -0,0 +1,83 @@
+﻿from enum import Enum
+from typing import Optional, List
+from sqlmodel import Field, Relationship, SQLModel
+
+
+
+
+
+class StoryPreviewTextBase(SQLModel):
+    textColor: Optional[str]
+    title: Optional[str]
+    subTitle: Optional[str]
+    sizeBetweenTitles: Optional[float]
+
+    storyPreview_id: Optional[int] = Field(default=None, foreign_key="storypreview.id")
+
+class StoryPreviewText(StoryPreviewTextBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    storyPreview: Optional["StoryPreview"] = Relationship(back_populates="StoryPreviewText")
+
+class StoryPreviewTextRead(StoryPreviewTextBase):
+    id: int
+    
+#==============
+
+class StoryPreviewBase(SQLModel):
+    previewImageUrl: str = Field(default=None)
+
+    story_id: Optional[int] = Field(default=None, foreign_key="story.id")
+
+class StoryPreview(StoryPreviewBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+    story: Optional["Story"] = Relationship(back_populates="screen")
+
+  
+class StoryPreviewRead(StoryPreviewBase):
+    id: int
+    # previewImageUrl: 
+    previewText: StoryPreviewTextRead
+
+#==============
+
+class StoryScreenBase(SQLModel):
+    imageUrl: str = Field(default=None)
+    duration: Optional[int] = Field(default=15)
+
+    story_id: Optional[int] = Field(default=None, foreign_key="story.id")
+
+
+class StoryScreen(StoryScreenBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    story: Optional["Story"] = Relationship(back_populates="screen")
+
+
+class StoryScreenRead(StoryScreenBase):
+    id: int
+
+
+#==============
+    
+
+class AccessLevel(Enum):
+    onlyClatchTeam = "onlyClatchTeam"
+    allUsers = "allUsers"
+
+
+
+class StoryBase(SQLModel):
+    progressBarColor: str
+    statusBarColor: str
+    accessLevel: Optional[AccessLevel] = Field(default=AccessLevel.allUsers)
+
+class Story(StoryBase, table=True):
+    id: int = Field(default=None, primary_key=True)
+    preview: Optional[StoryPreview] = Relationship(back_populates="story")
+    storyScreens: Optional[List["StoryScreen"]] = Relationship(back_populates="story")
+
+  
+class StoryRead(StoryBase):
+    id: int
+    storyScreens: List[StoryScreenRead]
+    preview: StoryPreviewRead
